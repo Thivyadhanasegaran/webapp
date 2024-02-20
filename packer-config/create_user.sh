82f2abe5e2ab9b09761cd23490249161fe2a6357
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-02-19 20:09:57 -0500

    Added packer code

--- packer-config/create_user.sh
+++ packer-config/create_user.sh
@@ -4,7 +4,8 @@
 sudo groupadd csye6225
 sudo useradd -r -s /usr/sbin/nologin -g csye6225 csye6225
 sudo chown -R csye6225:csye6225 -R /opt/webapp/
-sudo chmod +x server.js
+sudo chmod 755 /opt/webapp
+# sudo chmod +x server.js
 
 
 
