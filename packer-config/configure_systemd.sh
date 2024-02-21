dd1b2e4b7c0fc10629b359882c3a9fa3275f648a
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-02-21 18:36:49 -0500

    Final_Assignment04

--- packer-config/configure_systemd.sh
+++ packer-config/configure_systemd.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
-# sudo systemctl daemon-reload
-# sudo systemctl enable webapp
-# sudo systemctl start webapp
-# sudo systemctl restart webapp
-
 
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
  
