29d48907c8729f6badd06c7c00e25e1f2f2abb75
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-02-21 16:24:57 -0500

    Updated custom image code

--- packer-config/create_user.sh
+++ packer-config/create_user.sh
@@ -9,7 +9,7 @@ cd /opt/csye6225/webapp || exit
 sudo unzip webapp.zip
 sudo rm webapp.zip
 sudo groupadd -f csye6225
-sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
+sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225/webapp -m csye6225/webapp
 
 sudo chown -R csye6225:csye6225 /opt/csye6225/
 sudo chmod -R 775 /opt/csye6225 
