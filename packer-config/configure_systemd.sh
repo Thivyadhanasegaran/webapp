fa6d2c93769c5aa28d3db276c4bd5d44e39c0b5f
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-03-17 15:19:52 -0400

    Added ops-agent and logger

--- packer-config/configure_systemd.sh
+++ packer-config/configure_systemd.sh
@@ -8,6 +8,7 @@ sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
 sudo chmod 750 /etc/systemd/system/webapp.service
 sudo chown -R csye6225:csye6225 /opt/
 sudo chmod -R 750 /opt/webapp
+
  
  
 sudo systemctl daemon-reload
