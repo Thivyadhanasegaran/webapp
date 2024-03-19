b0eedcd437c86022f99e6730aaa7f1c06e21025b
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-03-19 18:29:09 -0400

    Added ops-agent and logger

--- logger/logger.js
+++ logger/logger.js
@@ -64,39 +64,38 @@ import winston from 'winston';
 import dotenv from "dotenv";
 dotenv.config();
 
-// Define custom severity levels
-const customLevels = {
-  levels: {
-    error: 0,
-    warn: 1,
-    info: 2,
-    debug: 3
-  }
-};
 
-// Register the custom severity levels with Winston
-winston.addColors(customLevels.levels);
+// const logger = winston.createLogger({
+//   level: 'info', 
+//   format: winston.format.combine(
+//     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), 
+//     winston.format.printf(info => JSON.stringify({ message: info.message, time: info.timestamp })) 
+
+//   ),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: process.env.LOGPATH ?? './log/webapp/app.log'})
+//   ]
+// });
+
+// const logger = winston.createLogger({transports: [
+//         new winston.transports.Console(),
+//         new winston.transports.File({ filename: process.env.LOGPATH ?? './log/webapp/app.log'})
+//       ]});
+// export default logger;
 
 const logger = winston.createLogger({
-  level: 'info', // Default level set to 'info'
-  levels: customLevels.levels, // Custom severity levels
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Customizing timestamp format
-    // winston.format.printf(info => JSON.stringify({ level: info.level, time: info.timestamp, message: info.message })) // Customizing log message format to JSON
-    winston.format.printf(info => JSON.stringify({ message: info.message, time: info.timestamp })) // Customizing log message format to include only message and time
-
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: process.env.LOGPATH ?? './log/webapp/app.log'})
-  ]
-});
-
-
-
-export default logger;
-
-
+    format: winston.format.combine(
+      winston.format.timestamp(), // Include timestamp
+      winston.format.json() // Log in JSON format
+    ),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: process.env.LOGPATH ?? './log/webapp/app.log'})
+    ]
+  });
+  
+  export default logger;
 
 
 
