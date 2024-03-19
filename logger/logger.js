ca953a2d90711acab2776962b0bc9f93c48045a6
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-03-19 15:52:34 -0400

    Added ops-agent and logger

--- logger/logger.js
+++ logger/logger.js
@@ -23,6 +23,43 @@
 // export default logger;
 
 
+// import winston from 'winston';
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// // Define custom severity levels
+// const customLevels = {
+//   levels: {
+//     error: 0,
+//     warn: 1,
+//     info: 2,
+//     debug: 3
+//   }
+// };
+
+// Register the custom severity levels with Winston
+// winston.addColors(customLevels.levels);
+
+// const logger = winston.createLogger({
+//   level: 'info', // Default level set to 'info'
+//   levels: customLevels.levels, // Custom severity levels
+//   format: winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.json(),
+//     winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+//   ),
+//   transports: [
+//     new winston.transports.Console(),
+//     new winston.transports.File({ filename: process.env.LOGPATH ?? './log/webapp/app.log'})
+//   ]
+// });
+
+// logger.log('info', 'This is an informational message.');
+// logger.log('error', 'This is an error message.');
+
+// export default logger;
+
+
 import winston from 'winston';
 import dotenv from "dotenv";
 dotenv.config();
@@ -44,9 +81,8 @@ const logger = winston.createLogger({
   level: 'info', // Default level set to 'info'
   levels: customLevels.levels, // Custom severity levels
   format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json(),
-    winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Customizing timestamp format
+    winston.format.printf(info => JSON.stringify({ level: info.level, time: info.timestamp, message: info.message })) // Customizing log message format to JSON
   ),
   transports: [
     new winston.transports.Console(),
@@ -55,9 +91,10 @@ const logger = winston.createLogger({
 });
 
 logger.log('info', 'This is an informational message.');
-logger.log('error', 'This is an error message.');
+
 
 export default logger;
+
 
 
 
