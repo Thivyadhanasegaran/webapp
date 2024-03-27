01896b819791b143b18e7a305023b4378d236083
Thivya Dhanasegaran <dhanasegaran.t@northeastern.edu>
2024-03-26 21:07:21 -0400

    Added verify_email logic

--- routes/verifyEmail.js
+++ routes/verifyEmail.js
@@ -6,6 +6,8 @@ import checkDBConnection from "../middlewares/checkDBConnection.js";
 import { handlePayload, validateQueryParams } from "../middlewares/checkPayloadAndQueryParams.js";
 
 // Define route for handling GET request
-router.get("/verify-email", validateQueryParams, handlePayload, checkDBConnection, basicAuth, verifyEmail);
+// router.get("/verify-email", validateQueryParams, handlePayload, checkDBConnection, basicAuth, verifyEmail);
+
+router.get("/verify-email", verifyEmail);
 
 export default router;
